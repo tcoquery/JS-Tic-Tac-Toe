c570e0eba52063b7e1a16d15b05ec63991893fc3
tcoquery <tcoquery@gmail.com>
2022-06-10 12:04:36 +0200

    Add board and game logic

---
+++ script.js
@@ -0,0 +1,60 @@
+const btn = document.querySelector("#btn");
+
+const player = (name, marker) => {
+    return {name, marker};
+};
+
+const gameBoard = (() => {
+    let board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    const displayBoard = () => {
+        for (let i = 1; i < 10; i++) {
+            let grid = document.getElementById(i);
+            grid.textContent = board[i-1];
+        }
+    }
+    const updateBoard = (n) => {
+        if(gameFlow.round == 1) {
+            if(board[n-1] == "X" || board[n-1] == "O") {
+                alert("This cell is already occupied !");
+            } else { 
+                board[n-1] = player1.marker;
+                gameFlow.round++;
+                displayBoard();
+            }            
+        } else {
+            if(board[n-1] == "X" || board[n-1] == "O") {
+                alert("This cell is already occupied !");
+            } else {
+                board[n-1] = player2.marker;
+                gameFlow.round--;
+                displayBoard();
+            }
+        }
+    }
+    return {board, displayBoard, updateBoard};
+})();
+
+const gameFlow = (() => {
+    let round = 1;
+    return {round};
+})();
+
+let grid = document.querySelectorAll(".grid-cell")
+grid.forEach(cell =>{
+    cell.addEventListener("click", function() {
+        gameBoard.updateBoard(cell.getAttribute('data-value'));
+    });
+});
+
+btn.addEventListener("click", () => {
+    if(document.getElementById('player-1').value == "" || document.getElementById('player-2').value == "") {
+        alert("Player's names cannot be empty.");
+        return;
+    } else {
+        const player1 = player(document.getElementById('player-1').value, "X"); 
+        const player2 = player(document.getElementById('player-2').value, "O");
+        document.querySelector(".player-info").style.visibility = "hidden";
+    }
+});
+
+gameBoard.displayBoard();
