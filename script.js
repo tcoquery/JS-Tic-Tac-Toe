2e4b2ce6b014b87b016f6eb271fa0eecf0ce37f5
tcoquery <tcoquery@gmail.com>
2022-06-10 12:18:23 +0200

    moved players' info to a different object

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const player = (name, marker) => {
 
 const gameBoard = (() => {
     let board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    let player1 = {};
-    let player2 = {};
     const displayBoard = () => {
         for (let i = 1; i < 10; i++) {
             let grid = document.getElementById(i);
@@ -19,7 +17,7 @@ const gameBoard = (() => {
             if(board[n-1] == "X" || board[n-1] == "O") {
                 alert("This cell is already occupied !");
             } else { 
-                board[n-1] = gameBoard.player1.marker;
+                board[n-1] = gameFlow.player1.marker;
                 gameFlow.round++;
                 displayBoard();
             }            
@@ -27,18 +25,20 @@ const gameBoard = (() => {
             if(board[n-1] == "X" || board[n-1] == "O") {
                 alert("This cell is already occupied !");
             } else {
-                board[n-1] = gameBoard.player2.marker;
+                board[n-1] = gameFlow.player2.marker;
                 gameFlow.round--;
                 displayBoard();
             }
         }
     }
-    return {board, player1, player2, displayBoard, updateBoard};
+    return {board, displayBoard, updateBoard};
 })();
 
 const gameFlow = (() => {
     let round = 1; 
-    return {round};
+    let player1 = {};
+    let player2 = {};
+    return {round, player1, player2};
 })();
 
 let grid = document.querySelectorAll(".grid-cell")
@@ -53,12 +53,10 @@ btn.addEventListener("click", () => {
         alert("Player's names cannot be empty.");
         return;
     } else {
-        gameBoard.player1 = player(document.getElementById('player-1').value, "X"); 
-        gameBoard.player2 = player(document.getElementById('player-2').value, "O");
+        gameFlow.player1 = player(document.getElementById('player-1').value, "X"); 
+        gameFlow.player2 = player(document.getElementById('player-2').value, "O");
         document.querySelector(".player-info").style.visibility = "hidden";
-        
     }
 });
 
 gameBoard.displayBoard();
-console.log(gameBoard.board)
